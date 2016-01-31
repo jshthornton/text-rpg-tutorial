b40382e79b5833f64d2242b8e23624f6536414f5
Lee Joscelyne <leojosc@gmail.com>
2016-01-31 17:56:15 +0000

    Display Class creation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,4 @@
+import Display from './components/Display';
 console.log("Hello World");
+var display = new Display(document.getElementById('display'));
+display.render();
--- src/assets/js/components/Display.js
+++ src/assets/js/components/Display.js
@@ -2,10 +2,10 @@
 
 export default class {
   constructor(el) {
-
+  	this.__el=el
   }
 
   render() {
-
+	this.__el.innerHTML = 'Hello';
   }
 };
